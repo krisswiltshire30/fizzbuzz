4cfcf99c39d6c0c80d7a7f3541b15100f1db723d
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-07-19 17:26:08 +0100

    All tests passed - Fizzbuzz complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,15 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-    "fizz"
+#If the number divided by 3 and 5 indipendtanly has a remainder of 0 then return 'fizzbuzz'
+  if num % 3 == 0 && num % 5 == 0
+    "fizzbuzz"
+#If the number divided by 5 has a remainder of 0 then return 'buzz'
   elsif num % 5 == 0
     "buzz"
+#If the number divided by 3 has a remainder of 0 then return 'fizz'
+  elsif num % 3 == 0
+    'fizz'
   else
-    return num
+#if none of the above apply then return the number
+     num
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -25,5 +25,40 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
 
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+
+  it 'returns 8 when passed 8' do
+    expect(fizzbuzz(8)).to eq 8
+  end
+
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns 11 when passed 11' do
+    expect(fizzbuzz(11)).to eq 11
+  end
+
+  it 'returns "fizz" when passed 12' do
+    expect(fizzbuzz(12)).to eq 'fizz'
+  end
+
+  it 'returns 13 when passed 13' do
+    expect(fizzbuzz(13)).to eq 13
+  end
+
+  it 'returns 14 when passed 14' do
+    expect(fizzbuzz(14)).to eq 14
+  end
+
+  it 'returns fizzbuzz when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 
 end
